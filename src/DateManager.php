eb8efe59617a1ad4bbf14be59866a6d9b76ec8f6
Aurimas Bachanovičius <aurimas.bachanovicius@metasite.net>
2017-12-12 11:43:08 +0200

    Initial

---
+++ src/DateManager.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * (c) Aurimas Bachanovicius <aurimas.bachanovicius@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace aurimasb\DateManager;
+
+/**
+ * Class DateManager
+ *
+ * @package aurimasb\DateManager
+ */
+class DateManager
+{
+
+    /**
+     * @param int $timestamp
+     * @return string
+     */
+    public static function timestampToDate(int $timestamp): string
+    {
+        return date('Y-m-d', $timestamp);
+    }
+}
